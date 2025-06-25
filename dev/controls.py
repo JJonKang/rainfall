60d9d6d4609830d1694bac6a2c754f2fa3cf1529
JJonKang <jjonkang03@gmail.com>
2025-06-25 18:28:19 -0500

    More controls and basic player added

--- dev/controls.py
+++ dev/controls.py
@@ -1,4 +1,23 @@
-class Movement:
+import pygame
+class Player:
     #for now the multipler for speed is simply 1
-    def __init__(self, spd_mult = 1):
+    def __init__(self, x = 0, y = 0,spd_mult = 2.5):
+        self.x = x
+        self.y = y
         self.spd_mult = spd_mult
+
+    def movement(self, key):
+        if key[pygame.K_d] or key[pygame.K_RIGHT]:
+            self.x += self.spd_mult
+        if key[pygame.K_a] or key[pygame.K_LEFT]:
+            self.x -= self.spd_mult
+        if key[pygame.K_w] or key[pygame.K_UP]:
+            self.y -= self.spd_mult
+        if key[pygame.K_s] or key[pygame.K_DOWN]:
+            self.y += self.spd_mult
+    
+    def object(self, obj):
+        pygame.draw.rect(obj, (25, 25, 2), (self.x, self.y, 25, 25))
+
+    def get_pos(self):
+        print(self.x, self.y, self.spd_mult)
--- dev/main.py
+++ dev/main.py
@@ -4,17 +4,24 @@ import controls as controls
 from sys import exit #true exit program
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("something special")
 icon = pygame.image.load('images/rain.jpg')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
+player = controls.Player()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    screen.fill('black')
     buttons.intro(screen)
+    #controlling the player
+    keys = pygame.key.get_pressed()
+    player.movement(keys)
+    player.object(screen)
+    player.get_pos()
     pygame.display.update()
     clock.tick(60)
